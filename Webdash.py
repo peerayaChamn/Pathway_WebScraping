f62a155aaa873f729562976becbaea93291106a0
peerayac <peerayac1997@outlook.com>
2021-03-16 11:18:02 -0600

    adding

--- Webdash.py
+++ Webdash.py
@@ -51,6 +51,7 @@ app.layout = html.Div([
                      options=[{'label': x, 'value': x} for x in df.Country_name.unique()],
                      multi=False,
                      disabled=False,
+                     value = 'Brazil',
                      clearable=True,
                      searchable=True,
                      placeholder="Country",
@@ -89,12 +90,14 @@ app.layout = html.Div([
                            clearable=True,
                            persistence='string',
                            style={'width': '50%'},
+                           value = 'accounting',
                            persistence_type='session'),
 
               dcc.Dropdown(id='country1',
                            options=[{'label': x, 'value': x} for x in df1.Country.unique()],
                            multi=False,
                            clearable=True,
+                           value = 'Canada',
                            persistence='string',
                            style={'width': '50%'},
                            persistence_type='session')
@@ -157,16 +160,21 @@ def build_graph(first, second):
 def build_graph1(first1, second2):
     dff = df[(df['Country_name'] == first1) &
              (df['Job_type'].isin(second2))]
-    fig = px.pie(dff, values='Total', names='Job_type', hole=.3)
+    fig = px.pie(dff, values='Total', names='Job_type', hole=.5, color_discrete_sequence=["#3A929D", "#FFC328", "#6ABDC8", "#B88300", '#AB63FA', '#FFA15A', '#19D3F3','#FF6692', '#B6E880', '#FF97FF', '#FECB52', '#EF553B', '#00CC96'])
     return fig
 
 
 @app.callback(
     dash.dependencies.Output('map', 'figure'),
     dash.dependencies.Input('country', 'value'))
+
 def build_graph2(first2):
     mask = df["Country_name"] == first2
-    fig = px.bar(df[mask], x="Total", y="Job_type", barmode="group", height=600)
+    fig = px.bar(df[mask], x="Total", y="Job_type", barmode="group", height=600, color_discrete_sequence=["#3A929D"],labels={
+        "Total": "Total Entry level jobs",
+        "Job_type": ""
+    })
+    fig.update_layout(plot_bgcolor="white")
     return fig
 
 
@@ -191,9 +199,11 @@ def build_graph3(country1, job1, value):
               (df1['job_type'] == job1)]
     skills = []
     if(job1=='Public Health'):
-        skills = ['Communication','Teamwork','Initiative','Interpersonal','Analytics','Flexibility','health']
+        skills = ['communication','teamwork','initiative','interpersonal','analytics','flexibility','health', 'Health']
     elif(job1=='Database'):
-        skills = ['Problem-solving','Communication','Oracle','SQL','PowerShell','Security','Analytics','database']
+        skills = ['problem-solving','communication','Oracle','SQL','PowerShell','security','analytic','database']
+    elif(job1=='accounting'):
+        skills = ['microsoft','QuickBooks','payable','receivable', '10 key','data entry','bookkeeping','organize','tax','deadline','security']
     skills.append(value)
 
     tags_df = dff["summary"].apply(clean_data)
@@ -204,7 +214,8 @@ def build_graph3(country1, job1, value):
     new = pd.DataFrame(filter_series)
     new = new.reset_index()
     new.columns = ['Name', 'Code']
-    fig = px.bar(new, x="Name", y="Code", barmode="group")
+    fig = px.bar(new, x="Name", y="Code", barmode="group",color_discrete_sequence=["#3A929D", "#FFC328", "#6ABDC8", "#B88300", '#AB63FA', '#FFA15A', '#19D3F3','#FF6692', '#B6E880', '#FF97FF', '#FECB52', '#EF553B', '#00CC96'])
+    fig.update_layout(plot_bgcolor="white")
     return fig
 
 @app.callback(
@@ -213,9 +224,11 @@ def build_graph3(country1, job1, value):
 
 def build_graph3(job1):
     if(job1 == 'Public Health'):
-        text = "Communication,Teamwork,Initiative,Interpersonal,Analytics,Flexibility,health"
+        text = 'communication, teamwork, initiative, interpersonal, analytics, flexibility, health, Health'
     if(job1=='Database'):
-        text = "Problem-solving,Communication,Oracle,SQL,PowerShell,Security,Analytics,database"
+        text = 'Problem-solving,Communication,Oracle,SQL,PowerShell,Security,Analytics,database'
+    if(job1=='accounting'):
+        text = "['microsoft','QuickBooks','payable','receivable', '10 key','data entry','bookkeeping','organize','tax','deadline','security']"
 
     return text
 
@@ -233,7 +246,7 @@ def build_graph4(country1, job1):
     text = " ".join(review for review in dff.summary)
 
     stopwords = set(STOPWORDS)
-    stopwords.update(["support"])
+    stopwords.update(["with","will","from","within","for","which"])
 
     # Generate a word cloud image
     wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
