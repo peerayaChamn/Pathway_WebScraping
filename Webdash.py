19b7ddac3b7ff89d9b9bc27d7ffbc138f2a70c00
peerayac <peerayac1997@outlook.com>
2021-03-16 10:04:25 -0600

    add

--- Webdash.py
+++ Webdash.py
@@ -9,28 +9,42 @@ Created on Fri Feb 19 11:30:39 2021
 import pandas as pd  # (version 1.0.0)
 import plotly  # (version 4.5.0)
 import plotly.express as px
+import dash_bootstrap_components as dbc
+from collections import Counter
+import plotly.graph_objects as go
+import re
+from wordcloud import WordCloud, STOPWORDS
 
+import nltk
+from nltk import word_tokenize
+
+from nltk.corpus import stopwords
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
 
 # from pandas.io import gbq
 
 # import pandas_gbq
 # reading from bigquery
-#df = gbq.read_gbq("select * from `parabolic-hook-303116.Jobs.Daily_scraping`", project_id="parabolic-hook-303116")
+# df = gbq.read_gbq("select * from `parabolic-hook-303116.Jobs.Daily_scraping`", project_id="parabolic-hook-303116")
 df = pd.read_csv("scraping_data.csv")
-df = df.sort_values('Posted_date')
+df['Posted_date'] = pd.to_datetime(df.Posted_date)
+df.sort_values(by='Posted_date')
+# df = df.sort_values('Posted_date')
+df = df.drop_duplicates(subset=['Job_type', 'Posted_date', 'Country_name'])
+df1 = pd.read_csv("all_info.csv")
+
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 server = app.server
 
 app.layout = html.Div([
+
     html.Div([
         html.H1('Jobs Information'),
- html.Div([
-    html.Img(src='/assets/img.png')
-]),
+        html.Div([
+            html.Img(src='/assets/img.png')
+        ]),
         html.Br(),
         html.Label([''], style={'font-weight': 'bold', "text-align": "center"}),
         dcc.Dropdown(id='country',
@@ -61,6 +75,53 @@ app.layout = html.Div([
         dcc.Graph(id='our_graph')
     ], className='search'),
 
+    html.Div([
+        dcc.Graph(id='x-time-series')
+    ], style={'display': 'inline-block', 'width': '49%', 'margin-top': '30%'}),
+
+    html.Div([
+        dcc.Graph(id='map')
+    ], style={'display': 'inline-block', 'width': '49%', 'margin-top': '40%'}),
+
+    html.Div([dcc.Dropdown(id='job1',
+                           options=[{'label': x, 'value': x} for x in df1.job_type.unique()],
+                           multi=False,
+                           clearable=True,
+                           persistence='string',
+                           style={'width': '50%'},
+                           persistence_type='session'),
+
+              dcc.Dropdown(id='country1',
+                           options=[{'label': x, 'value': x} for x in df1.Country.unique()],
+                           multi=False,
+                           clearable=True,
+                           persistence='string',
+                           style={'width': '50%'},
+                           persistence_type='session')
+
+              ], className='NLP'),
+
+    html.Div(
+        [
+            html.Br(),
+            dcc.Input(id="input1", type="text", placeholder=""),
+        ]
+    ),
+
+    html.Div([
+        dcc.Graph(id='NLP')
+    ], style={'display': 'inline-block', 'width': '49%', 'margin-top': '10%'}),
+
+    html.Div([
+        dcc.Graph(id='NLP1')
+    ], style={'display': 'inline-block', 'width': '49%', 'margin-top': '10%'}),
+
+    html.Div(
+        [
+            html.Div(id="output"),
+        ]
+    ),
+
 ])
 
 
@@ -71,12 +132,14 @@ app.layout = html.Div([
 def build_graph(first, second):
     dff = df[(df['Country_name'] == first) &
              (df['Job_type'].isin(second))]
-    fig = px.line(dff, x="Posted_date", y="Total", color="Job_type",labels={
-                     "Posted_date": "Date",
-                     "Total": "Total Entry level jobs",
-                     "Job_type": "Certificates"
-                 },
-                title="Total Job Posting by Day",color_discrete_sequence=["#3A929D", "#FFC328", "#6ABDC8","#B88300",'#AB63FA', '#FFA15A', '#19D3F3', '#FF6692', '#B6E880', '#FF97FF', '#FECB52','#EF553B', '#00CC96'])
+    fig = px.line(dff, x="Posted_date", y="Total", color="Job_type", labels={
+        "Posted_date": "Date",
+        "Total": "Total Entry level jobs",
+        "Job_type": "Certificates"
+    },
+                  title="Total Job Posting by Day",
+                  color_discrete_sequence=["#3A929D", "#FFC328", "#6ABDC8", "#B88300", '#AB63FA', '#FFA15A', '#19D3F3',
+                                           '#FF6692', '#B6E880', '#FF97FF', '#FECB52', '#EF553B', '#00CC96'])
     fig.update_layout(plot_bgcolor="white")
     fig.update_traces(line=dict(width=4))
     fig.update_layout(
@@ -84,10 +147,101 @@ def build_graph(first, second):
         title_font_family="Arial",
         title_font_color="#999999",
     )
+    return fig
 
+
+@app.callback(
+    dash.dependencies.Output('x-time-series', 'figure'),
+    [dash.dependencies.Input('country', 'value'),
+     dash.dependencies.Input('job', 'value')])
+def build_graph1(first1, second2):
+    dff = df[(df['Country_name'] == first1) &
+             (df['Job_type'].isin(second2))]
+    fig = px.pie(dff, values='Total', names='Job_type', hole=.3)
+    return fig
+
+
+@app.callback(
+    dash.dependencies.Output('map', 'figure'),
+    dash.dependencies.Input('country', 'value'))
+def build_graph2(first2):
+    mask = df["Country_name"] == first2
+    fig = px.bar(df[mask], x="Total", y="Job_type", barmode="group", height=600)
+    return fig
+
+
+stop_words = stopwords.words('english')
+
+
+def clean_data(desc):
+    desc = word_tokenize(desc)
+    desc = [word.lower() for word in desc if word.isalpha() and len(word) > 2]
+    desc = [word for word in desc if word not in stop_words]
+    return desc
+
+
+@app.callback(
+    dash.dependencies.Output('NLP', 'figure'),
+    [dash.dependencies.Input('country1', 'value'),
+     dash.dependencies.Input('job1', 'value'),
+     dash.dependencies.Input('input1', 'value')
+     ])
+def build_graph3(country1, job1, value):
+    dff = df1[(df1['Country'] == country1) &
+              (df1['job_type'] == job1)]
+    skills = []
+    if(job1=='Public Health'):
+        skills = ['Communication','Teamwork','Initiative','Interpersonal','Analytics','Flexibility','health']
+    elif(job1=='Database'):
+        skills = ['Problem-solving','Communication','Oracle','SQL','PowerShell','Security','Analytics','database']
+    skills.append(value)
+
+    tags_df = dff["summary"].apply(clean_data)
+    result = tags_df.apply(Counter).sum().items()
+    result = sorted(result, key=lambda kv: kv[1], reverse=True)
+    result_series = pd.Series({k: v for k, v in result})
+    filter_series = result_series.filter(items=skills)
+    new = pd.DataFrame(filter_series)
+    new = new.reset_index()
+    new.columns = ['Name', 'Code']
+    fig = px.bar(new, x="Name", y="Code", barmode="group")
+    return fig
+
+@app.callback(
+    dash.dependencies.Output('output', 'children'),
+    [dash.dependencies.Input('job1', 'value')])
+
+def build_graph3(job1):
+    if(job1 == 'Public Health'):
+        text = "Communication,Teamwork,Initiative,Interpersonal,Analytics,Flexibility,health"
+    if(job1=='Database'):
+        text = "Problem-solving,Communication,Oracle,SQL,PowerShell,Security,Analytics,database"
+
+    return text
+
+from wordcloud import WordCloud, STOPWORDS
+
+
+@app.callback(
+    dash.dependencies.Output('NLP1', 'figure'),
+    [dash.dependencies.Input('country1', 'value'),
+     dash.dependencies.Input('job1', 'value')])
+def build_graph4(country1, job1):
+    dff = df1[(df1['Country'] == country1) &
+              (df1['job_type'] == job1)]
+
+    text = " ".join(review for review in dff.summary)
+
+    stopwords = set(STOPWORDS)
+    stopwords.update(["support"])
+
+    # Generate a word cloud image
+    wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+
+    # Display the generated image:
+    fig = px.imshow(wordcloud)
     return fig
 
 
 if __name__ == '__main__':
     app.run_server(debug=False)
-
